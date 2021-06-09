51244da77d8b2c5892e3aa4168496721b9c8b40c
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-06-09 16:24:27 -0600

    Add function that takes a list of lasp_packets.parser.Packet objects and creates a structured numpy array, preferentially using derived values but falling back to raw values in their absence. Add test for array_from_packets.

---
+++ libera_sdp/io/packets.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cde39c1b645f7471a17aece294824670f1d03c8374078f8a261b1ad30734c6e9
+size 1866
---
+++ tests/test_io/test_packets.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:424a1c0f711069f9512cd2a2fd11eec2087fa46e9e85635105bd59a5a78a5ee2
+size 2931
