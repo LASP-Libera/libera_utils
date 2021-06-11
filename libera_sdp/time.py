22901c55f8fd98fd2f497db572a20de04cc71742
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-06-11 11:24:41 -0600

    Add some time conversion utility functions.

---
+++ libera_sdp/time.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcb662c53516555e5d9cd3810439ec6ac8b4a9704682b357521e049ca834e085
+size 2204
---
+++ tests/test_time.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccbcc99e2eae2542726cd0fbc9d5e13462869a1c1289046f3d557317825cd788
+size 2895
