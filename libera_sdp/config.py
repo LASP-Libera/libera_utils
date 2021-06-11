bcccc243b22152f4864af53610bfbf9096f7b4c9
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-06-10 19:26:12 -0600

    Add configuration JSON file to repo and reuse code from previous missions for retrieving JSON or environment variables.

---
+++ libera_sdp/config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2224854e75aa2d36905b0830afb0d1bc8100d25cca76514a2a8fb5d04b0d40b3
+size 2704
---
+++ tests/test_config.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6b0348305c9064631945c011f5454ab5d923f855c1d3a28417388abeede190df
+size 404
