7e9890b1544029273336e4748ebcb5eb7ebe42c4
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-08-03 13:31:25 -0600

    Clean up minor things like formatting of import statements. Add logging facility to kernel creation CLI entrypoints. Add a SCLK for JPSS-3

--- libera_sdp/cli/kernel_maker.py
+++ libera_sdp/cli/kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba52586f82d5f398d7c57d86434ea5458b97a2bd447cfd8e90fb902d15f9bdad
-size 6310
+oid sha256:fa10b3a1eaa2df030190566ed9560fb15b1553e1a1591d7bf6ea56fc5620133a
+size 8665
