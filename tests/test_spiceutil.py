9e9ee868c02cc0005deb9e213ab20c79d25bc253
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-10-03 15:03:04 -0600

    Add additional failure condition test for fallback kernel to KernelFileCache test. Modify fake naif index page to list multiple files for more realism. Fix bug in KernelFileCache.cache_dir for linux system Add logging on fallback kernel usage.

--- tests/test_spiceutil.py
+++ tests/test_spiceutil.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a2c4df35cef5c8e1d62d705ed5bd0aef20b3f68d28c9ae7bd3811dca00dbe3c
-size 5801
+oid sha256:9d47c1a259365e67708304435fc1d405c6250ef9597c4ab60721b5730d8c211b
+size 6845
