78be12111e58468c947bfbea09f46816f4c2cd9f
Gavin Medley <gavin.medley@lasp.colorado.edu>
2021-11-19 17:58:14 -0700

    Refactor CLI application to a single entrypoint with subcommands (similar to the AWS CLI or docker CLI). Update README with new commands Refactor modules to reflect the new organization of CLI code. Add new fixture that clears the SPICE pool after every test to ensure auto-furnished spice kernels don't interfere with future tests. Add version module that provides a function to get the package version using importlib.

---
+++ tests/test_cli.py
@@ -0,0 +1,33 @@
+"""Tests for cli module"""
+# Installed
+import argparse
+import pytest
+# Local
+from libera_sdp import cli, kernel_maker
+
+
+@pytest.mark.parametrize(
+    ("cli_args", "parsed"),
+    [
+        (['make-kernel', 'jpss-spk', 'file1.pkts', 'file2.pkts'],
+         argparse.Namespace(
+             func=kernel_maker.make_jpss_spk,
+             packet_data_filepaths=['file1.pkts', 'file2.pkts'],
+             outdir='/tmp',
+             verbose=False,
+             overwrite=False)),
+        (['make-kernel', 'jpss-ck', 'file1.pkts', 'file2.pkts'],
+         argparse.Namespace(
+             func=kernel_maker.make_jpss_ck,
+             packet_data_filepaths=['file1.pkts', 'file2.pkts'],
+             outdir='/tmp',
+             verbose=False,
+             overwrite=False)),
+        (['--version'],
+         argparse.Namespace(
+             func=cli.print_version_info)
+         ),
+    ]
+)
+def test_parse_cli_args(cli_args, parsed):
+    assert dict(vars(cli.parse_cli_args(cli_args))) == dict(vars(parsed))
