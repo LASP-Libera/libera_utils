57033bab6810eab5ebaf544d080ad31137ae6076
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2022-01-12 10:50:57 -0700

    added modules for swath hdf-eos5 filehandling

---
+++ libera_sdp/io/hdfeos_swath.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f35fcdd3470f096a7df9f653ae8db4fb9ee9cab47807829616e55910a2a7eb7a
+size 6152
---
+++ tests/test_io/test_hdfeos_swath.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27008c093c58257b878133f7ea212b75a91513152b50260c3d0f7b47884531cf
+size 2760
