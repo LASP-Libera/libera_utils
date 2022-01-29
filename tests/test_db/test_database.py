0705350ee713fe654e530e4e43903a0cf414b6f1
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-01-29 13:02:33 -0700

    Lots of TODOs left from this. Fix those in next commit. Add baseline schema and ORM objects. Add mixin stub classes for querying data products in common ways and representing DB objects nicely. Add some tests for DB ORM models.

--- tests/test_db/test_database.py
+++ tests/test_db/test_database.py
@@ -7,55 +7,55 @@ import pytest
 # Local
 from libera_sdp.db import getdb
 from libera_sdp.db.database import DatabaseException
-from libera_sdp.db.models import *
+from libera_sdp.db.models import L0
 
 
-def db_testfunc(filename: str = None, ingest_complete: bool = None):
+def db_testfunc(filename: str = None, version: int = None):
     """DB test function that retrieves some dummy data"""
     db = getdb()
     print(f"Hello from pid {os.getpid()}. db.pid={db.pid}. db.url={db.url}")
     with db.session() as s:
-        records = s.query(Level0)
+        records = s.query(L0)
         if filename is not None:
-            records = records.filter(Level0.filename == filename)
-        if ingest_complete is not None:
-            records = records.filter(Level0.ingest_complete == ingest_complete)
+            records = records.filter(L0.filename == filename)
+        if version is not None:
+            records = records.filter(L0.version == version)
         return records.all()
 
 
 def test_db_connection(insert_test_data, clean_sdp_test_db):
     """Tests ability to connect to the test DB without doing anything special
     This ensures that the pytest fixture that sets the test DB is in place"""
-    insert_test_data(Level0(filename='foofile.txt', ingest_complete=True))
+    insert_test_data(L0(filename='foofile.txt', version=1))
     with getdb().session() as s:
-        records = s.query(Level0).all()
+        records = s.query(L0).all()
     assert len(records) == 1
     assert records[0].filename == 'foofile.txt'
-    assert records[0].ingest_complete is True
+    assert records[0].version == 1
 
 
 def test_db_manager_multiprocessing(insert_test_data, clean_sdp_test_db):
     """Test that the module level DB manager object works in multiprocessing"""
     insert_test_data(
-        Level0(filename='foofile.txt', ingest_complete=True),
-        Level0(filename='barfile.txt', ingest_complete=True),
-        Level0(filename='bazfile.txt', ingest_complete=True),
-        Level0(filename='zipfile.txt', ingest_complete=False),
-        Level0(filename='zapfile.txt', ingest_complete=False),
-        Level0(filename='zopfile.txt', ingest_complete=False)
+        L0(filename='foofile.txt', version=1),
+        L0(filename='barfile.txt', version=1),
+        L0(filename='bazfile.txt', version=1),
+        L0(filename='zipfile.txt', version=0),
+        L0(filename='zapfile.txt', version=0),
+        L0(filename='zopfile.txt', version=0)
     )
 
     for method in ('spawn', 'fork'):
         with multiprocessing.get_context(method).Pool(4) as pool:
             res = pool.starmap(db_testfunc,
                                [
-                                   ('foofile.txt', True),
+                                   ('foofile.txt', 1),
                                    ('zipfile.txt', None),
                                    ('bazfile.txt', None),
-                                   (None, False),
-                                   (None, True),
-                                   ('barfile.txt', True),
-                                   ('barfile.txt', False)
+                                   (None, 0),
+                                   (None, 1),
+                                   ('barfile.txt', 1),
+                                   ('barfile.txt', 0)
                                ])
 
 
