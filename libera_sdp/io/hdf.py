c01594e078166351385ee20841d3e546e55df041
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-02-21 14:11:46 -0700

    Modify h5dump to return a string. Add an optional parameter to h5dump to print to stdout in addition to returning a string.

--- libera_sdp/io/hdf.py
+++ libera_sdp/io/hdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fda7bd32d65951670a4b0aef7df62e9f19f6de979f3bfa75981742aba5fbe375
-size 1018
+oid sha256:e6eb6d39714e94d4e81eddebc875bd04a29cf637ea07a8ca90c0bc2a94e3eb67
+size 1527
--- tests/test_io/test_hdf.py
+++ tests/test_io/test_hdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b651896a874ffc01504e6cc5cc081e8bdc3e29c80ac1c76d3d57f7adb67c302d
-size 1353
+oid sha256:2d9eca5e1e2a162ed93b60a8a7fcdd72f3d918b03730769b81bfb45726869b84
+size 1442
