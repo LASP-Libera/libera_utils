b9051886ca484a1e272d93dfaed9a7804ae9763e
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2022-03-22 15:06:53 -0600

    minor edits

--- libera_sdp/io/hdfeos_swath.py
+++ libera_sdp/io/hdfeos_swath.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc9adb8b07eb717b52e09dfca5225493f6b27cf17aa3a51b5873b2a9b2e7dbb0
-size 8055
+oid sha256:a9cecb88ca340133fe8d0e163e438efa2c1da2b111a8590efa1f22739c4bc4cb
+size 7580
--- tests/test_io/test_hdfeos_swath.py
+++ tests/test_io/test_hdfeos_swath.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4118646fcf4f71317c8149ad4999cdc5ce8e451ec645abfc76fae9b1eed1af2d
-size 2560
+oid sha256:8c443039014525d5d4cc281139ee16368385dcc6328fa57e5ca0fa89286f7dc0
+size 2591
