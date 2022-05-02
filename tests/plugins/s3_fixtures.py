d509da219379a28c460c6c1554a8e249f03b13c6
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-05-02 16:23:31 -0600

    Suppress exception during mocking AWS_PROFILE if it doesn't exist in the environment

--- tests/plugins/s3_fixtures.py
+++ tests/plugins/s3_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9199435f0e703dfbfcc5cde690f16fc567ab604f5cd53ca838aaadf9ab6fad6c
-size 2798
+oid sha256:e0e1ea9d3d4714546b169f92902182e76e7b6fd679aac1bac029ef5c6c430a6b
+size 2813
