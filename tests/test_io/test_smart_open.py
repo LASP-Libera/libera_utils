443caaf080faa937c54c86326aa1e4dd1a122db8
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2022-06-02 14:08:24 -0600

    now uses test_hdf5 in test_smart_open_mode

--- tests/test_io/test_smart_open.py
+++ tests/test_io/test_smart_open.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a2078e2b78173f9ab5cca4ce0304a7eaf8dfa5a9fc5efad45cd464aa97d9ca1
-size 4617
+oid sha256:490088191c35b3884ce9495e7d31668d7cf448f2d7e41e8481ba46923a626f44
+size 4463
