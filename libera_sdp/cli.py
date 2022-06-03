2d69c73e7bac904d8120468fd9c7fde4bbc9485b
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-06-03 12:17:35 -0600

    Move kernel generator tests to integration directory Remove default `/tmp` directory from CLI because it's bad practice Require --outdir to be specified in CLI Update tests

--- libera_sdp/cli.py
+++ libera_sdp/cli.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40c02d67b978d3efa83077baac491aba990d8df7fb202ea740ae549b1bf24a80
-size 4259
+oid sha256:42035bee14db83dfaf896a8c7f1a09f15babc75880dd8d51339865c9fd1ba348
+size 4371
