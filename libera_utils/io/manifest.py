b26a35a8f9a4e0c50a83978c042a65ad2543c647
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-07-13 20:43:00 -0600

    Tiny docstring edit

--- libera_utils/io/manifest.py
+++ libera_utils/io/manifest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99a6bcfcb15dd2da5d3a292d157be8a14342207162cb7de9bd6d4ca5034a5aaf
-size 3031
+oid sha256:0fdb3a8ade2153f157efd8b725c31dc0c1cfb963a2cf66fe3acab0f6e4566e99
+size 3038
