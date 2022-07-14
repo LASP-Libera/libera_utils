07e366cc54a6de001dd2348606b9f25a09057b66
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-07-13 22:04:54 -0600

    Add warning to is_s3 of it is passed a Path containing an s3 url.

--- libera_utils/io/smart_open.py
+++ libera_utils/io/smart_open.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b908ebba2d0f771f719a609346be860e9f3e7ed78b0f448673b077fb70b11d5c
-size 2596
+oid sha256:bacdf7ee512020ec35bfebe56cf3a489fa0ee50bd8b800e455baac646b09c857
+size 2773
