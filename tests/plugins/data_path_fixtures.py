97d62d5bfc39c7c1e8e382ba14e38df1e336c119
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-07-13 20:08:56 -0600

    Add manifest.py module Add test_manifest.py unit tests Update our "manifest/PCF" test data file to include "manifest_type" key.

--- tests/plugins/data_path_fixtures.py
+++ tests/plugins/data_path_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e528ad5c2b5589d606c917fedc7356ca4e534ef5af39b01ed94be8b586d6609
-size 2822
+oid sha256:d4f577e09207d0b99344c96ded7c97675682794d0dbfd43a2fbc72e1e27838e7
+size 2991
---
+++ tests/unit/test_io/test_manifest.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:834c89ea5fa564ac6e6dbea9c6737517d76f30186d1834f64161c893e7bef83c
+size 695
