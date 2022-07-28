02184e6c764788d09e6661a53ee0bb7b8080b1eb
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-07-28 10:10:58 -0600

    Monkeypatch AWS_REGION AND AWS_DEFAULT_REGION for testing (mock_aws_credentials)

--- tests/plugins/s3_fixtures.py
+++ tests/plugins/s3_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0e1ea9d3d4714546b169f92902182e76e7b6fd679aac1bac029ef5c6c430a6b
-size 2813
+oid sha256:5bb72b262d9eff964591692b325a6b1e1e0f8ef4f0917ac48cba070fef51dc9c
+size 2941
