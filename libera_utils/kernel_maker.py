c8a23afae1055475e2bbff0a45fa1dbf08faa5e5
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-09-22 21:42:05 -0600

    Fix deliberate omission that was checking for failure behavior.

--- libera_utils/kernel_maker.py
+++ libera_utils/kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79a7f674843bdcf5c39cc676415bb7ff60607117869ce085df077821400751ca
-size 12231
+oid sha256:58ac103fa9be6ec6101a7c96df0e0508d98cf6942aedbbe423e161d0a431d556
+size 12236
