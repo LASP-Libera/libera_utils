a857e4154e60de6813cb29bfc8c88dd031e8d0bd
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-09-22 21:35:03 -0600

    Updated # nosec exceptions for Bandit.

--- libera_utils/db/database.py
+++ libera_utils/db/database.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a36a0cbcec083b013e848cea780fe35c205b1cce7c2d30dab51263801974df42
-size 5996
+oid sha256:3f6c4ed7839bbe1af2830cde675b3ddad396089747e5a404e4f1c664a53b7727
+size 6003
--- libera_utils/kernel_maker.py
+++ libera_utils/kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:047665d4ded5df42c04f5e809d1593396b95b0047ed858f74deae1dc2df7bf27
-size 12169
+oid sha256:79a7f674843bdcf5c39cc676415bb7ff60607117869ce085df077821400751ca
+size 12231
