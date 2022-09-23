a04a1e4743ec86f8c51f775c795ebc86cd09de34
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-09-22 20:22:56 -0600

    Remove assert statements controlling logic since they get removed when compiled down to bytecode (Bandit warning).

--- libera_utils/io/hdf.py
+++ libera_utils/io/hdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20dd491a25bb46051e52dd59c20e9153397b3c44ed12f5abc6f51d20d46ff0fe
-size 9237
+oid sha256:898da5d412ce36d0c16b47079607d648e82d51fb8cc64e1f8bd2487ce26ddf4f
+size 9272
