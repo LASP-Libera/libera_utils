f83f7e8a0e703a609ddefb4d0970c0ce3cf863a1
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-10-06 10:25:59 -0600

    Fix integration tests usage of configure_task_logging

--- libera_utils/kernel_maker.py
+++ libera_utils/kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:820fc87328e32a2a62d5d8e6d00228b261a835e126dee910429d0dbabe224037
-size 11928
+oid sha256:e8e7854cdf4fc18a20994ebd6d4180f799113e06adf153e687fdfd88de439827
+size 12120
