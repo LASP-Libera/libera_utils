690474e01be89db998e1c75417aec6df848af969
Gavin Medley <gavin.medley@lasp.colorado.edu>
2022-11-16 15:36:14 -0700

    Remove HDF-EOS code, tests, and configuration attributes.

--- tests/unit/test_io/test_hdf.py
+++ tests/unit/test_io/test_hdf.py
@@ -1,10 +1,8 @@
 """Tests for hdf module"""
 # Installed
 import h5py as h5
-import numpy as np
 import pytest
 # Local
-from libera_utils.config import config
 from libera_utils.io import hdf
 
 
@@ -66,86 +64,3 @@ def test_h5dump(minimal_h5, capsys, group, kwargs, expected_string):
             assert captured.out == expected_string
         else:
             assert captured.out == ""
-
-
-@pytest.fixture()
-def test_attributes(tmp_path):
-    """Test dictionary"""
-
-    dir = tmp_path / "sub"
-    dir.mkdir()  # Ensure directory exists
-    path = dir / "swath_test.he5"
-
-    testdict = {
-        'path': str(path),
-        'attribute_path': config.get("LIBSDP_ATTR"),
-        'swath_names': ['Swath1'],
-        'dataset_path': 'HDFEOS/SWATHS/Swath1/DataField',
-        'dataset_names': ['Temperature', 'SunglintAngle'],
-        'datasets': [np.array([1, 1]), np.array([2, 2])],
-        'dataset_units': ['Kelvin', 'radians']
-    }
-
-    return testdict
-
-
-def test_create_swath_groups(test_attributes):
-    """Test ability to add groups"""
-
-    hdfeos = hdf.SwathHdfEos5(test_attributes['path'], test_attributes['attribute_path'], mode="w")
-
-    hdfeos.create_swath_groups(['Swath1', 'Swath2'])
-    f = hdf.h5dump(h5.File(test_attributes['path']))
-
-    assert 'Swath1' in f
-
-
-def test_add_swath_datasets(test_attributes):
-    """Test ability to add datasets and metadata"""
-
-    hdfeos = hdf.SwathHdfEos5(test_attributes['path'], test_attributes['attribute_path'], mode="w")
-
-    hdfeos.create_swath_groups(['Swath1', 'Swath2'])
-
-    dataset_path_1 = 'HDFEOS/SWATHS/Swath1/DataField'
-    dataset_path_2 = 'HDFEOS/SWATHS/Swath2/DataField'
-    dataset_names = ['Temperature', 'SunglintAngle']
-    datasets = [np.array([1, 1]), np.array([2, 2])]
-    dataset_units = ['Kelvin', 'radians']
-
-    hdfeos.add_swath_dataset(dataset_path_1, dataset_names, datasets, dataset_units)
-    hdfeos.add_swath_dataset(dataset_path_2, dataset_names, datasets, dataset_units)
-
-    dataset_path_2 = 'HDFEOS/SWATHS/Swath2/GeoField'
-    dataset_names = ['scantime', 'tai93time']
-    datasets = [np.array([1, 1]), np.array([2, 2])]
-    dataset_units = ['ugps', 'sec']
-
-    hdfeos.add_swath_dataset(dataset_path_2, dataset_names, datasets, dataset_units)
-    hdfeos.add_swath_metadata()
-
-    f = hdf.h5dump(h5.File(test_attributes['path']))
-
-    assert 'Temperature' in f
-    assert 'tai93time' in f
-
-
-def test_add_swath_file_attr(test_attributes):
-    """Test file attributes"""
-
-    hdfeos = hdf.SwathHdfEos5(test_attributes['path'], test_attributes['attribute_path'], mode="w")
-
-    hdfeos.add_swath_file_attr()
-
-    f = hdf.h5dump(h5.File(test_attributes['path']))
-
-    assert 'Level 2 Libera Data' in f
-
-
-def test_validate(test_attributes):
-    """Test file attributes"""
-
-    try:
-        hdf.SwathHdfEos5.validate(test_attributes)
-    except AssertionError as err:
-        print(err)
