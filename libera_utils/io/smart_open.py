f3538480662b977911cf1553c5594d8ad5e1bed4
Matt Watwood <Matt.Watwood@lasp.colorado.edu>
2022-12-01 13:40:30 -0700

    Updating again to pass all kernel maker tests and using is_dir for local path to determine proper naming.

--- libera_utils/io/smart_open.py
+++ libera_utils/io/smart_open.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:410da712fe4496e03796b1f9cbce3a7b7ad853f2c9bac3fee1b36d1930e5e378
-size 4945
+oid sha256:ca26fab92ca2d7be1c7ec9ac3eaca309db2993b222c665c11b43a0b2127c0627
+size 4966
--- libera_utils/kernel_maker.py
+++ libera_utils/kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:712ce53c53c6df49017228ec02dc7f73adbe2ea2c1fd68f626574d5e5ed8d2a6
-size 12706
+oid sha256:ad17150fc6721800348b87f357d49a3b4a8383d258b2c6efca95823fa8508960
+size 12913
