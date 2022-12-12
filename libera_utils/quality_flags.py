e42a980a9042038dd5fc1f7e1e3d488f96af0a3b
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2022-12-12 13:41:37 -0700

    modified QF heritage code

--- libera_utils/quality_flags.py
+++ libera_utils/quality_flags.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85c5e74c475d34f1ff8701b55a3a7cfaa514d1c6e977ce50f82721807b1804b0
-size 13102
+oid sha256:0c5ddf6fba627149972e2e9ee4a0e6d4bcb84d546d771c9027dabc17bbc3f0f3
+size 8476
---
+++ tests/unit/test_quality_flags.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0f557ce756c35b0407bdf4b0159c5ff4fc2e5d22df84225c38fad3674f166b3
+size 1861
