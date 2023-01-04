a62dfe2c551115a7cd9eed77d2ec49d4ead95b72
Matt Watwood <Matt.Watwood@lasp.colorado.edu>
2023-01-04 13:36:12 -0700

    Fixing local path issues in manifest

--- tests/integration/test_kernel_maker.py
+++ tests/integration/test_kernel_maker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ddfd07075faa43436e504dbfd287d90e9a67b42dff2b005b2571aab5686dc96
-size 11413
+oid sha256:46ebe73eb17ee55826d32cc1f92a0e745a9da2b781336fe10bda8842431193ee
+size 12924
