bf280db082e8b511a445b1aff948e5e29fafff1c
Caleb Kumar <kumarad@macl4333.lasp.colorado.edu>
2023-01-11 13:36:12 -0700

    Add License and Copyright info to version output

--- libera_utils/cli.py
+++ libera_utils/cli.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c371c3815e4379e68d6266d13d4c83a03daeb69c8ceb4b11dc8f0424f1348803
-size 4227
+oid sha256:3e6d0aac05532192888fa29e78597a296f9418b887a4dc8189b0984ec27b6e69
+size 4299
