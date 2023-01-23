647e4280044dcea2e1de37bc0a0afa420cfadb16
Matt Watwood <Matt.Watwood@lasp.colorado.edu>
2023-01-23 09:45:13 -0700

    Full construction record reading with some basic unit tests

--- libera_utils/db/models.py
+++ libera_utils/db/models.py
@@ -21,6 +21,7 @@ class Cr(Base, ReprMixin):
     edos_software_version = Column(INTEGER)
     construction_record_type = Column(INTEGER)
     test_flag = Column(BOOLEAN)
+    n_scs_start_stops = Column(INTEGER)
     n_bytes_fill_data = Column(NUMERIC(20))
     n_length_mismatches = Column(BIGINT)
     first_packet_sc_time = Column(NUMERIC(20))
@@ -32,10 +33,12 @@ class Cr(Base, ReprMixin):
     n_rs_corrections = Column(BIGINT)
     n_packets = Column(BIGINT)
     size_bytes = Column(NUMERIC(20))
+    n_ssc_discontinuities = Column(INTEGER)
     completion_time = Column(NUMERIC(20))
     n_apids = Column(SMALLINT)
     n_pds_files = Column(SMALLINT)
 
+    scs_start_stop_times: list = relationship("CrScsStartStopTimes", back_populates="construction_record")
     apids: list = relationship("CrApid", back_populates="construction_record")
     pds_files: list = relationship("PdsFile", back_populates="construction_record")
 
@@ -49,11 +52,25 @@ class CrApid(Base, ReprMixin):  # I know...
     scid_apid = Column(BIGINT)
     byte_offset = Column(NUMERIC(20))
     n_vcids = Column(SMALLINT)
-    n_ssc_discontinuities = Column(BIGINT)
+    n_ssc_gaps = Column(BIGINT)
+    n_edos_generated_fill_data = Column(BIGINT)
+    count_edos_generated_octets = Column(NUMERIC(20))
+    n_length_discrepancy_packets = Column(BIGINT)
+    first_packet_sc_time = Column(NUMERIC(20))
+    last_packet_sc_time = Column(NUMERIC(20))
+    first_packet_utc_time = Column(TIMESTAMP(timezone=True))
+    last_packet_utc_time = Column(TIMESTAMP(timezone=True))
+    esh_first_packet_time = Column(NUMERIC(20))
+    esh_last_packet_time = Column(NUMERIC(20))
+    n_vcdu_corrected_packets = Column(BIGINT)
+    n_in_the_data_set = Column(BIGINT)
+    n_octect_in_apid = Column(NUMERIC(20))
 
     construction_record = relationship("Cr", back_populates="apids")
     vcids: list = relationship("CrApidVcid", back_populates="apid")
     ssc_gaps: list = relationship("CrApidSscGap", back_populates="apid")
+    ssc_length_discrepancies: list = relationship("CrApidSscLenDiscrepancies", back_populates="apid")
+    edos_fill_data: list = relationship("CrApidEdosGeneratedFillData", back_populates="apid")
 
 
 class CrApidVcid(Base, ReprMixin):
@@ -113,6 +130,44 @@ class PdsFileApid(Base, ReprMixin):
     last_packet_utc_time = Column(TIMESTAMP(timezone=True))
 
     pds_file = relationship("PdsFile", back_populates="apids")
+
+
+class CrScsStartStopTimes(Base, ReprMixin):
+    """
+    SSC Start and Stop Time information for a Construction Record
+    """
+    id = Column(INTEGER, primary_key=True, server_default=FetchedValue())
+    cr_id = Column(INTEGER, ForeignKey(Cr.id))
+    scs_start_sc_time = Column(NUMERIC(20))
+    scs_stop_sc_time = Column(NUMERIC(20))
+    scs_start_utc_time = Column(TIMESTAMP(timezone=True))
+    scs_stop_utc_time = Column(TIMESTAMP(timezone=True))
+
+    construction_record = relationship("Cr", back_populates="scs_start_stop_times")
+
+
+class CrApidSscLenDiscrepancies(Base, ReprMixin):
+    """
+    SSC length discrepancies per APID in a CR
+    """
+    id = Column(INTEGER, primary_key=True, server_default=FetchedValue())
+    cr_apid_id = Column(INTEGER, ForeignKey(CrApid.id))
+    ssc_length_discrepancy = Column(BIGINT)
+
+    apid = relationship("CrApid", back_populates="ssc_length_discrepancies")
+
+
+class CrApidEdosGeneratedFillData(Base, ReprMixin):
+    """
+    EDOS generated fill data per APID in a CR
+    """
+    id = Column(INTEGER, primary_key=True, server_default=FetchedValue())
+    cr_apid_id = Column(INTEGER, ForeignKey(CrApid.id))
+    ssc_with_generated_data = Column(BIGINT)
+    filled_byte_offset = Column(NUMERIC(20))
+    index_to_fill_octet = Column(BIGINT)
+
+    apid = relationship("CrApid", back_populates="edos_fill_data")
 
 
 class SpkCkFile(Base, ReprMixin, DataProductMixin):
