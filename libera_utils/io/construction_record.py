e3fd93c94d27933910507a9acf8caddf0ddb0e1c
Matt Watwood <Matt.Watwood@lasp.colorado.edu>
2023-01-23 10:33:00 -0700

    Pylint fixes

--- libera_utils/io/construction_record.py
+++ libera_utils/io/construction_record.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f5602eebc2092c40bdaecc5dd66b07665ef6d2152132ac7049ccba729186bf
-size 13973
+oid sha256:6d5551d9d690283e2c273a531d723078dd26e3fb86d24a743d7e19dd2975beec
+size 14784
