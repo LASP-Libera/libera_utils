a08bca0ef5ee9d9746b9f10e6aaa14a2d31fd9ee
Matt Watwood <Matt.Watwood@lasp.colorado.edu>
2023-01-24 14:05:52 -0700

    Pylint fixes including one pylint ignore for an AnyPath error.

--- libera_utils/io/construction_record.py
+++ libera_utils/io/construction_record.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6718dd78b841bc8ad3168b605345a6fa6178b2e7a1ac7090326407754b63c9d
-size 20918
+oid sha256:1209a60a3f7d293de1f1e9cefd9d959a5a6bc64632c8c852a930b06365b7ba57
+size 22117
