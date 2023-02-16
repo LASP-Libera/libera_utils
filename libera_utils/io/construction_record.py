6cb800bee0086e2f1a406e4449caced7d06e7a5c
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-02-16 10:05:13 -0700

    updates to the construction record parser for pds record insertion separate from cr insertion

--- libera_utils/io/construction_record.py
+++ libera_utils/io/construction_record.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05a79dee4ff78381d2ceb19b4f2bd684cdaab7bd00ed11584cc9b166d85622d3
-size 21143
+oid sha256:b493c31918717d6e9097c79d1646b9f079aa871be7cb8af893cb9c67a5a4341a
+size 22159
