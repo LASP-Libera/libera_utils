80c108fc26f445d47d1ab365797c45e2452a827b
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-02-16 09:53:18 -0700

    update to data_path_fixtures.py

--- tests/plugins/data_path_fixtures.py
+++ tests/plugins/data_path_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c093ff1ae112f4b6d2e60601be87090290d1c78586e30b0864594ff5c1d60427
-size 3161
+oid sha256:e7e0e33501618c3473cf38608704dd8edb69fa19910f855229acad9ec80af8d0
+size 3353
