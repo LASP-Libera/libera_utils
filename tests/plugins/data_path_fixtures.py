6fee78cd7fa132f6570a058ab1f3d4e443b04a64
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-02-16 10:22:09 -0700

    added file to data path fixtures

--- tests/plugins/data_path_fixtures.py
+++ tests/plugins/data_path_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7e0e33501618c3473cf38608704dd8edb69fa19910f855229acad9ec80af8d0
-size 3353
+oid sha256:fd87506e2470cb80a46c55ec29bdf9ab4b5c03d9aa74f1fcbc304e3e9b011202
+size 3545
