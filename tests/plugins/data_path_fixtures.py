7cb0af11b5914d591dfd5b299e07810ecf46c25b
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-02-22 09:52:18 -0700

    added output manifest file to fixtures

--- tests/plugins/data_path_fixtures.py
+++ tests/plugins/data_path_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d7f78d835b684d137058b6345b6a15e3c33d8058460893aad3ea29c15cb107c
-size 3545
+oid sha256:3561f8b182ca0097930892f54f074a32f1d3a48d15f158eefbde2bddb16797be
+size 3706
