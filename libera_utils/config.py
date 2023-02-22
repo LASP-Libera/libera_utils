cba4c08ee75572487e9d4160a80edce1467a0655
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-02-22 09:49:48 -0700

    removed typo

--- libera_utils/config.py
+++ libera_utils/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fb5ac15464a92c941ed5c3854c96c9db0e359eebdfdbb8b4a67abddef972429
-size 4921
+oid sha256:6f6c2669a23b51bbe41db5d34061cfeda4c7d04d842f966efe97312e1a338aff
+size 4916
