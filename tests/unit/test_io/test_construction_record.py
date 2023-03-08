49bbd67dd6d72faff3da167ee7263011403cd1d2
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-03-08 14:26:53 -0700

    fixed typo

--- tests/unit/test_io/test_construction_record.py
+++ tests/unit/test_io/test_construction_record.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e1830556a2cb6929e25049e9d04642a227ef6a5e4a12764a7ac6ecd2ca34d60
+oid sha256:7b9ccdf92b09ff190814bdc46114d11272bd9f924db112f863dd4b589508659d
 size 2595
