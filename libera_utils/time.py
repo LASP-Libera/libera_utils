ad4d06158818acbfdc63afe78ecc722dacf816ef
Gavin Medley <gavin.medley@lasp.colorado.edu>
2023-04-20 15:18:20 -0600

    Tiny change to the way we handle formatting revision timestamps to use the common format string from time.py

--- libera_utils/time.py
+++ libera_utils/time.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a14ac151e578b2ba0e011599105ee71b832da9565a9c880680eb1d2de171db
-size 7482
+oid sha256:40f0bead1c6eb0437a6587af1fb9d0090748d72f170c7a3ecc2948737a350f57
+size 7489
