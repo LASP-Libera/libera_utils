d3dc0c4d4cadfeb70a83a28838f654b9615004cb
Gavin Medley <gavin.medley@lasp.colorado.edu>
2023-04-20 16:57:12 -0600

    Update test SPICE kernels to match our new naming conventions

--- tests/plugins/data_path_fixtures.py
+++ tests/plugins/data_path_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7d1b4704fcb1b28e0a68f277ffb64e5335c4801e9328a2356ccc18e63304fcd
-size 3382
+oid sha256:c04dd550915b749df38763ca8df6c7e37227be60212cd8d45f26bc910bd8def5
+size 3424
