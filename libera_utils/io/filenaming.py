0fcf3284d1b4a4ca680424019b60bc9446da8248
Gavin Medley <gavin.medley@lasp.colorado.edu>
2023-04-20 11:46:02 -0600

    Modify SPICE kernel naming to include a processing algorithm version string

--- libera_utils/io/filenaming.py
+++ libera_utils/io/filenaming.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2627655a89f43b2f7b382e74dae00ed03205beb4f551a68f2866e1994c5db9b6
-size 30836
+oid sha256:3545a18f6e65f5f14e26d1e82bd7bf62a21b3a2418bd8001611202d2d660f10b
+size 32093
--- tests/unit/test_io/test_filenaming.py
+++ tests/unit/test_io/test_filenaming.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:715e4b099b056d7c5ca34084811cdcd2ed1330142498e3c60b9ebe85d687610c
-size 12573
+oid sha256:dbed58bbdc78fcd0e4db2e8c3939648692d46e0e69c8ca4061d7b6e0c87a42eb
+size 12808
