e63b8c0005dd916e2e8a2759b9c43782b7804ea9
Matt Watwood <matt.watwood@lasp.colorado.edu>
2023-06-05 20:16:02 +0000

    Pull request #73: Fixing ORM model error with file name
    Merge in LIBSDC/libera_utils from hotfix/orm-model-repr-attr-printing to dev
    
    Squashed commit of the following:
    
    commit 41f794a03c37fa700849ab07dc50dd21d36fbf82
    Author: Matt Watwood <Matt.Watwood@lasp.colorado.edu>
    Date:   Fri May 26 06:56:02 2023 -0600
    
        Fixing ORM model error with file name
    
        Added a unit test for this. Only happens after using the ingest function. Using a direct .add method on an entry does not produce any errors.

--- tests/unit/test_io/test_packet_ingest.py
+++ tests/unit/test_io/test_packet_ingest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c649cb1d79ae1bd412dc05b36f80bde4c76f9c27e981db5d77c5535cbaf7665b
-size 7739
+oid sha256:dbca9cc5e5e7c9066596ccf83f5508fd05cc6c8c78948d8c181ec39c748b3b91
+size 8480
