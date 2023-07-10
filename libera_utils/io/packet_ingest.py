7e8f515c73a1049e1648fce85ab5db9bdc7080c2
Laura Sandoval <Laura.Sandoval@lasp.colorado.edu>
2023-07-10 19:21:05 +0000

    Pull request #75: path change edit
    Merge in LIBSDC/libera_utils from feature/LIBSDC-221-alpha-step-function-in-cdk to dev
    
    Squashed commit of the following:
    
    commit 815afbd923d606c55f7397273abebdefe29631cc
    Author: Matt Watwood <Matt.Watwood@lasp.colorado.edu>
    Date:   Mon Jul 10 13:10:35 2023 -0600
    
        Updating packet ingest to work with step functions
    
        Includes changes to the path in packet ingest using os.path and in the unit tests. This will be updated in the next few tickets.

--- libera_utils/io/packet_ingest.py
+++ libera_utils/io/packet_ingest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c5f11aafe7cd7b662d75779bd6e016b790ee8f2202ab268cdb5f285486054f7
-size 7486
+oid sha256:ad7292aabcdf88a936753f70dd70e5ee116cde5245b5bf2a40116bf1475b5ff9
+size 7501
--- tests/unit/test_io/test_packet_ingest.py
+++ tests/unit/test_io/test_packet_ingest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbca9cc5e5e7c9066596ccf83f5508fd05cc6c8c78948d8c181ec39c748b3b91
-size 8480
+oid sha256:2a18a87e38fc7b7d2901b3e27bd6b83105178b7dff0ee2c1614a481a997f3830
+size 8555
