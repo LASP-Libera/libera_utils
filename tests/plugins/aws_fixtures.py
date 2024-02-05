2d4d553e171cbf7674857d6a0dbbb761d2f63995
Matt Watwood <matt.watwood@lasp.colorado.edu>
2024-02-05 21:34:49 +0000

    Pull request #93: Feature/libera utils migrate jenkins pipeline scripts to git
    Merge in LIBSDC/libera_utils from feature/libera-utils-migrate-jenkins-pipeline-scripts-to-git to dev
    
    Squashed commit of the following:
    
    commit ed436361edd2291d3be05275851693dd8e4cd379
    Author: Matt Watwood <Matt.Watwood@lasp.colorado.edu>
    Date:   Thu Feb 1 15:40:00 2024 -0700
    
        Moving commented JenkinsFile to repo
    
        Hot fix for moto changes

--- tests/plugins/aws_fixtures.py
+++ tests/plugins/aws_fixtures.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf891fd99554c5c7ef7e6c8c8e6f2b72eeaadab4685c192a820309d5562ab79
-size 6350
+oid sha256:182606f8149048deae6998e06ac4986b3613236bf50b18b16c834ff981722cc6
+size 6309
