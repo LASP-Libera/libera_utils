b27cfa7136285e724e62a9bcc373a9ce0d5982fb
Gavin Medley <Gavin.Medley@lasp.colorado.edu>
2025-03-12 20:35:10 +0000

    Pull request #134: Refactor quality_flags module and remove old enum backport
    Merge in LIBSDC/libera_utils from flag-refactoring to main
    
    Squashed commit of the following:
    
    commit 540e7ac3fb6412e0c7abe7def8aa313e6c7528b5
    Author: Gavin Medley <gavin.medley@lasp.colorado.edu>
    Date:   Thu Mar 6 15:22:16 2025 -0700
    
        Refactor quality_flags module and remove old enum backport

--- tests/unit/test_quality_flags.py
+++ tests/unit/test_quality_flags.py
@@ -3,32 +3,59 @@
 import pytest
 # Local
 import libera_utils.quality_flags as qf
+from enum import verify, NAMED_FLAGS, UNIQUE
 
 
-def test_quality_flag_metaclass():
+def test_quality_flag_verify_unique():
     """
-    Test to ensure that the QualityFlagMeta metaclass successfully
-    prevents adding entries to a class created with the metaclass
+    Test to ensure that all bit masks are named uniquely
     """
-    class TestFlag(qf.QualityFlag, metaclass=qf.FrozenFlagMeta):
+
+    with pytest.raises(ValueError):
+        @verify(UNIQUE)
+        class TestFlag(qf.LiberaFlag):
+            BIT_0 = 0b001
+            BIT_1 = 0b010
+            BIT_2 = 0b100
+            BIT_1_AND_2 = 0b110
+            ALIAS = 0b100  # NOT unique
+
+def test_quality_flag_verify_named_flags():
+    """
+    Test to ensure that all individual bits are named in the enum before being referenced in
+    combined flag bit masks
+    """
+    with pytest.raises(ValueError):
+        @verify(NAMED_FLAGS)
+        class TestFlag(qf.LiberaFlag):
+            BIT_0 = 0b001
+            BIT_1 = 0b010
+            BIT_1_AND_2 = 0b110  # BIT 2 not named
+
+
+def test_quality_flag_member_immutability():
+    """Test that you can't change a member of a quality flag class later (you can still add members though)"""
+    @verify(UNIQUE, NAMED_FLAGS)
+    class TestFlag(qf.LiberaFlag):
         BIT_0 = 0b001
         BIT_1 = 0b010
         BIT_2 = 0b100
         BIT_1_AND_2 = 0b110
 
-    with pytest.raises(AttributeError):
-        TestFlag.FOOBAR = 0b111  # Can't add a new element after definition
+    with pytest.raises(ValueError):
+        TestFlag(99)  # Raises because 99 is out of range (violates STRICT boundary)
 
     with pytest.raises(AttributeError):
-        TestFlag.BIT_0 = 99  # Can't reassign element after definition
+        TestFlag.BIT_0 = 0b111  # Can't reassign element after definition
+
+    TestFlag.FOOBAR = 0b001
 
     assert TestFlag.BIT_2 | TestFlag.BIT_1 == TestFlag.BIT_1_AND_2
 
 
 def test_quality_flag():
     """Test our ability to create summary messages from a quality flag"""
-    @qf.with_all_none
-    class TestFlag(qf.QualityFlag, metaclass=qf.FrozenFlagMeta):
+    class TestFlag(qf.LiberaFlag):
         A = qf.FlagBit(0b1, message="Bit 0 - A")
         B = qf.FlagBit(0b10, message="Bit 1 - B")
         C = qf.FlagBit(0b100, message="Bit 2 - C")
@@ -36,16 +63,16 @@ def test_quality_flag():
 
     assert (TestFlag.A | TestFlag.B).decompose() == ([TestFlag.B, TestFlag.A], 0)
 
-    assert TestFlag.A & TestFlag.B == TestFlag.NONE
+    assert TestFlag.A & TestFlag.B == TestFlag(0)
 
-    assert bool(TestFlag.NONE) is False
+    assert bool(TestFlag(0)) is False
 
     assert bool(TestFlag.A) is True
 
     f0 = TestFlag.A | TestFlag.B
     f1 = TestFlag.A | TestFlag.B | TestFlag.C
     assert f1 & f0 == TestFlag.A | TestFlag.B
-    assert f1 & TestFlag.A & TestFlag.B == TestFlag.NONE
+    assert f1 & TestFlag.A & TestFlag.B == TestFlag(0)
 
     f2 = TestFlag.A | TestFlag.C
     assert f2 & f0 == TestFlag.A
@@ -55,13 +82,13 @@ def test_quality_flag():
     f = TestFlag.A | TestFlag.B
     assert set(f.summary[1]) == {"Bit 0 - A", "Bit 1 - B"}
 
-    all = TestFlag.ALL
-    assert set(all.summary[1]) == {"Bit 0 - A", "Bit 1 - B", "Bit 2 - C", "D"}
+    all_ = ~TestFlag(0)
+    assert set(all_.summary[1]) == {"Bit 0 - A", "Bit 1 - B", "Bit 2 - C", "D"}
 
 
 def test_strict_quality_flags():
     """Test that quality flags are STRICT and raise errors for invalid values"""
-    class TestFlag(qf.QualityFlag, metaclass=qf.FrozenFlagMeta):
+    class TestFlag(qf.LiberaFlag):
         BIT_0 = 0b001
         BIT_2 = 0b100
         # Note: there is no way to represent the number 2 or 3 with this quality flag since it is missing bit 1
