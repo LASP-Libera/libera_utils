7303a8301d86890aa2190bb29d0988244f2c861a
Gavin Medley <gavin.medley@lasp.colorado.edu>
2026-01-15 19:01:08 +0000

    Fix Error Handling in JSON Logging System
    We use a custom JSON log formatter to format log messages as JSON
    strings for Cloudwatch. We were getting some logging system errors
    previously, which were frustrating because nothing in the code was
    wrong, just the log message wasn't being formatted successfully and that
    is a BAD reason to break code.
    
    This bugfix adds robust error handling with effective fallbacks around
    the JSON logging system so that objects that are unserializable never
    break the logging system.

--- libera_utils/logutil.py
+++ libera_utils/logutil.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ceb9e8cea2c22183358379f115644cab158c87a160d95c7fb6958d2c67196581
-size 11040
+oid sha256:c942df70bbfaab0401d810a1d927e5c4b498310ae9e11260b1e6ae23f940e90c
+size 16451
