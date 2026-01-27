b247433357397aa70f38513119ec09fe71d019a7
Matt Watwood <matt.watwood@lasp.colorado.edu>
2026-01-27 21:56:07 +0000

    Pull request #195: Improving netcdf writing
    Merge in LIBSDC/libera_utils from hotfix/netcdf-writer-fix to main
    
    Squashed commit of the following:
    
    commit 8ae5d34d7fd6ab4a8f94b7bbef9f40019a9cc52f
    Author: Matt Watwood <Matt.Watwood@lasp.colorado.edu>
    Date:   Tue Jan 27 13:23:00 2026 -0700
    
        PR response
    
    commit b40f0d0bb60712a35a3205396073a8b151137ebe
    Author: Matt Watwood <Matt.Watwood@lasp.colorado.edu>
    Date:   Sat Jan 24 12:27:38 2026 -0700
    
        Improving netcdf writing
        
        Clarified different calls for different NetCDF engines
        Improved h5netcdf writing for S3 writing

--- libera_utils/io/netcdf.py
+++ libera_utils/io/netcdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cdb2c806257e3124eddf8a798b33e7f625d1c82cc4adec9f41587c05601b1f8
-size 5326
+oid sha256:e2dd05f3140922d21a7300ee697ee75de6c84e7658172ebf06f16feab4426cac
+size 5581
--- tests/unit/test_io/test_netcdf.py
+++ tests/unit/test_io/test_netcdf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3c86d7812de1af15b0989dfa2c21d14be5705c218902b21d20fc57eeee23a77
-size 26992
+oid sha256:d3ba5b3bab8040444644185978ddeda8589f08ab5ff27368ecfb7b38dc50f755
+size 28849
