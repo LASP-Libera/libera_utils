895a4c8b8ec28e43c36ced1e7922f3bd77dc45f5
Gavin Medley <gavin.medley@lasp.colorado.edu>
2026-02-05 23:46:53 +0000

    Fix Ruff Check Errors
    The UMM G module was using multiple inheritance to create string enums
    instead of StrEnum. Ruff called it out when it was updated to 0.15.

--- libera_utils/io/umm_g.py
+++ libera_utils/io/umm_g.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df5adc5aff4f2761a4b2ae8bee852e1ca9611dca21ffe7b049eb41d3e1c799f8
-size 63341
+oid sha256:6ae87728409e0251eb857f47bd0d4a712111a71b946efe10b7561b8253b21677
+size 63310
